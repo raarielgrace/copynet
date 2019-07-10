8ad6c3e15d944fc4d1d08db58a36f784a1f4ec6b
Deniz Bayazit <ariel_rotter-aboyoun@brown.edu>
2019-07-10 09:43:34 -0400

    Added north one location data.

--- ltldataset.py
+++ ltldataset.py
@@ -36,8 +36,8 @@ class Language(object):
 class SequencePairDataset(Dataset):
     #src_data_path='./data/hard_pc_src_syn2.txt'
     #tgt_data_path='./data/hard_pc_tar_syn2.txt'
-    src_data_path='./data/north_src.txt'
-    tgt_data_path='./data/north_tar.txt'
+    src_data_path='./data/north_one_lm_src.txt'
+    tgt_data_path='./data/north_one_lm_tar.txt'
 
     with open(src_data_path, "r") as sf:
         src_lines = sf.readlines()
